0e00f6441e698d9ceffdbf80913ced3f2b766df4
drdynscript <drdynscript@gmail.com>
2017-02-06 12:23:42 +0100

    WIP

---
+++ hello/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args == null | args.Length == 0)
+            {
+                Console.WriteLine("Hello World - For Developers Without arguments!");
+            }
+            else 
+            {
+                string msg = "";
+                for(var i = 0;i<args.Length;i++) 
+                {
+                    if(i > 0)
+                    {
+                        msg += " ";
+                    }
+                    msg += args[i];
+                }
+                
+                Console.WriteLine("Hello World - {0}!", msg);
+            }
+        }
+    }
+}
